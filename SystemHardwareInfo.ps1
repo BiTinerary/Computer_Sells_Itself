461f3b03141f1e3bdc95e3831ec579a9a9c419c1
Zack <Zack@bitinerary.com>
2015-03-20 14:04:37 -0500

    Adjust
    Adjusting and tinkering

--- SystemHardwareInfo.ps1
+++ SystemHardwareInfo.ps1
@@ -4,10 +4,12 @@ $OS = Get-CimInstance CIM_OperatingSystem
 $CPU = Get-CimInstance CIM_Processor
 $HDD = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'"
 $EXTXT = "$env:USERPROFILE\Desktop\welp.txt"
+ 
 Clear-Host
 
-"Manufacturer: " + $System.Manufacturer >> $EXTXT
-"Model: " + $System.Model >> $EXTXT
+$LISTMANU = "Model: " + $System.Manufacturer + " "
+$LISTMODEL = $System.Model($LISTMODEL.length - 4, 4)
+$LISTMANU + $LISTMODEL >> $EXTXT
 "CPU: " + $CPU.Name >> $EXTXT
 "RAM: " + "{0:N2}" -f ($System.TotalPhysicalMemory/1GB) + "GB" >> $EXTXT
 "HDD Capacity: "  + "{0:N2}" -f ($HDD.Size/1GB) + "GB" >> $EXTXT
