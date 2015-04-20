19f18d7404d78a7e92436ca71e2cbeb563cd5a04
Zack <Zack@bitinerary.com>
2015-04-20 11:32:40 -0500

    updated for firefox and selenium

--- Dependancies Installer.bat
+++ Dependancies Installer.bat
@@ -5,12 +5,15 @@ setx PATH "%PATH%;%HOMEDRIVE%\Windows\System32;%HOMEDRIVE%\Windows\System32\Wind
 setx PYTHON_PATH "%HOMEDRIVE%\Python27;%HOMEDRIVE%\\Python27\\Lib;%HOMEDRIVE%\\Python27\\DLLs;%HOMEDRIVE%\\Python27\\Scripts;%HOMEDRIVE%\\Python27\\Lib\\site-packages;%HOMEDRIVE%\\Python27\\Lib\\lib-tk" /m
 
 :: Install Chocolatey for automated prog install
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-choco install python -version 2.7.2 -y
-choco upgrade python -version 2.7.2 -y
-choco install pip -y
-choco install easy.install -y
-choco install pywin32 -y
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+chocolatey install firefox -y
+chocolatey install python -version 2.7.2 -y
+chocolatey upgrade python -version 2.7.2 -y
+chocolatey install pip -y
+chocolatey install easy.install -y
+chocolatey install pywin32 -y
 pip install wmi
+pip install selenium -U
 
 PAUSE
