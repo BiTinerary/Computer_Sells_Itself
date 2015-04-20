0a189705c5163a0000c3336bcd402e1ce146eeb8
Zack <Zack@bitinerary.com>
2015-04-20 10:30:27 -0500

    Fixed EnVar registry addition.
    Includes previously existing Environmental Variables in addition to
    ensureing the bare minimum. System32 folder, Powershell, etc... while
    adding envars for Python chocolatey, etc...

--- Dependancies Installer.bat
+++ Dependancies Installer.bat
@@ -1,16 +1,16 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco install python -version 2.7.2
-choco upgrade python -version 2.7.2
-choco install pip
-choco install pywin32
-choco install easy.install
-pip install wmi
+:: Set Path variable equal to existing path variables plus the previously mentioned
+setx PATH "%PATH%;%HOMEDRIVE%\Windows\System32;%HOMEDRIVE%\Windows\System32\WindowsPowerShell\v1.0;%HOMEDRIVE%\Python27;%HOMEDRIVE%\\Python27\\Lib;%HOMEDRIVE%\\Python27\\DLLs;%HOMEDRIVE%\\Python27\\Scripts;%HOMEDRIVE%\\Python27\\Lib\\site-packages;%HOMEDRIVE%\\Python27\\Lib\\lib-tk;%ALLUSERSPROFILE%\chocolatey\bin" /m
 
-:: Assign all Path variables
-SET PYTHON="%HOMEDRIVE%\Python27;%HOMEDRIVE%\\Python27\\Lib;%HOMEDRIVE%\\Python27\\DLLs;%HOMEDRIVE%\\Python27\\Scripts;%HOMEDRIVE%\\Python27\\Lib\\site-packages;%HOMEDRIVE%\\Python27\\Lib\\lib-tk"
-:: Set Path variable
-setx PATH "%PYTHON%" /m
 :: Set Python variable
 setx PYTHON_PATH "%HOMEDRIVE%\Python27;%HOMEDRIVE%\\Python27\\Lib;%HOMEDRIVE%\\Python27\\DLLs;%HOMEDRIVE%\\Python27\\Scripts;%HOMEDRIVE%\\Python27\\Lib\\site-packages;%HOMEDRIVE%\\Python27\\Lib\\lib-tk" /m
+
+:: Install Chocolatey for automated prog install
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+choco install python -version 2.7.2 -y
+choco upgrade python -version 2.7.2 -y
+choco install pip -y
+choco install easy.install -y
+choco install pywin32 -y
+pip install wmi
 
 PAUSE
